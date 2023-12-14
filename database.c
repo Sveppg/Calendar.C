70fcdee78e22197706ce4d46d5160f7432396014
Sveppg <terminator12327@gmail.com>
2023-12-14 18:28:50 +0100

    new file: unfinished

---
+++ database.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calendar.h"
+#include "datastructure.h"
+#include "datetime.h"
+#include "escapesequenzen.h"
+#include "menu.h"
+#include "tools.h"
+
+int saveCalendar()
+{
+    
+}
