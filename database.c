9c625af1945b8e140713c98a1d23896100d608fd
Sveppg <54738234+Sveppg@users.noreply.github.com>
2024-01-05 03:11:34 +0100

    Update database.c header

--- database.c
+++ database.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "calendar.h"
 
 // Funktion zum Speichern eines Termins in einer Datei
 void saveAppointment(FILE* file, Appointment* appointment) {
